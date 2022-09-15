c881743e4d601469d2b6e0643aa98ceb8b2b83bf
ivanjarkoi <85834009+ivanjarkoi@users.noreply.github.com>
2022-09-15 15:56:48 +0300

    Add files via upload

---
+++ MainScreen.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Prilojenie
+{
+    /// <summary>
+    /// Логика взаимодействия для MainScreen.xaml
+    /// </summary>
+    public partial class MainScreen : Page
+    {
+        public MainScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void Visitor_Click(object sender, RoutedEventArgs e)
+        {
+            Main.MainFrame.Navigate(new VisitorManu());
+        }
+    }
+}
